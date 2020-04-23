9c65e1cd0a8d07b0c33f3e8d488c376d6b9d30c4
zelbatmi <zakari_a@icloud.com>
2020-04-23 23:20:08 +0000

    v1.0

--- app/src/main/java/digiplus/ma/myuserprofile/MainActivity.java
+++ app/src/main/java/digiplus/ma/myuserprofile/MainActivity.java
@@ -2,7 +2,9 @@ package digiplus.ma.myuserprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View;
 
@@ -15,7 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
             TextView NameFiled = (TextView) findViewById(R.id.Name);
             NameFiled.setText("Zakaria");
+            NameFiled.setTextSize(30);
+            NameFiled.setTextColor(Color.RED);
+
+            ImageView ProfilePic = (ImageView) findViewById(R.id.Profile_Picture);
+            ProfilePic.setImageResource(R.drawable.profilepic);
+            TextView birthday = (TextView) findViewById(R.id.Birthday);
+            birthday.setText("23/04/2020");
+            TextView Country = (TextView) findViewById(R.id.Country);
+            Country.setText("Morocco");
+            Country.setTextSize(30);
         }
-    }
+        }
+
+
+
+
+
 
 
